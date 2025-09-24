cd894e55155c8f02d1e7b629ffb8304423aab6da
unknown <mukeshbhattampb111@gmail.com>
2025-09-25 00:27:32 +0545

    chore: add scripts/config for profiling

--- src/index.js
+++ src/index.js
@@ -5,12 +5,26 @@ dotenv.config();
 const hostname=process.env.HOSTNAME;
 const port =process.env.PORT;
 const server = http.createServer((req,res)=>{
+    
+if(req.method === 'GET'){
     res.writeHead(200,{'Content-Type':'application/json'});
-    res.write(JSON.stringify({message:"Server successfully created"}));
+    res.write(JSON.stringify({message:"This is the get request."}));
     res.end();
+
+}
+else{
+    res.writeHead(200,{'Content-Type':'application/json'});
+    res.write(JSON.stringify({message:"This is not the get request."}));
+    res.end();
+
+}
+
+
+
 })
 
 
 server.listen(port,hostname,()=>{
     console.log(`The server is listening at port ${port}`);
+    console.log(WebAssembly);
 })
